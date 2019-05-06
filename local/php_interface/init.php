71238337fd85ecb87bc1d49291ff38c65a44b085
vagrant <vagrant@homestead-bitrix>
2019-05-06 01:45:18 +0000

    edit readme file

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Автозагрузка классов проекта
+ */
 if (file_exists($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php')){
     require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 }
